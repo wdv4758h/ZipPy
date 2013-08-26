7f915ee18e30d0217cb32cfd2d880a384b23cf79
Mohaned Qunaibit <mohioq@gmail.com>
2013-08-26 02:22:01 -0700

    Error handler for the parser

--- graal/edu.uci.python.parser/src/edu/uci/python/antlr/ErrorHandlerMsg.java
+++ graal/edu.uci.python.parser/src/edu/uci/python/antlr/ErrorHandlerMsg.java
@@ -1,30 +1,53 @@
+/*
+ * Copyright (c) 2013, Regents of the University of California
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package edu.uci.python.antlr;
 
 import org.antlr.runtime.*;
-import org.python.antlr.ParseException;
 
 import edu.uci.python.nodes.*;
 import edu.uci.python.nodes.statements.*;
 
 public class ErrorHandlerMsg {
 
-    public PNode errorExpr(PNode t) {
+    public PNode errorExpr(Token t) {
         throw new ParseException("Bad Expr Node", t.getLine(), t.getCharPositionInLine());
     }
 
-    public PNode errorMod(PNode t) {
+    public PNode errorMod(Token t) {
         throw new ParseException("Bad Mod Node", t.getLine(), t.getCharPositionInLine());
     }
 
-    public PNode errorSlice(PNode t) {
+    public PNode errorSlice(Token t) {
         throw new ParseException("Bad Slice Node", t.getLine(), t.getCharPositionInLine());
     }
 
-    public StatementNode errorStmt(PNode t) {
+    public StatementNode errorStmt(Token t) {
         throw new ParseException("Bad Stmt Node", t.getLine(), t.getCharPositionInLine());
     }
 
-    public void error(String message, PNode t) {
+    public void error(String message, Token t) {
         throw new ParseException(message, t.getLine(), t.getCharPositionInLine());
     }
 
