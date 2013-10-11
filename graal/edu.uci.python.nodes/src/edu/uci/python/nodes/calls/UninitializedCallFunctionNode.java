5c77d8b2d000aa9e9ccdd42d8b547562cf15cbab
Wei Zhang <ndrzmansn@gmail.com>
2013-10-11 00:17:08 -0700

    Add self to PArguments

---
+++ graal/edu.uci.python.nodes/src/edu/uci/python/nodes/calls/UninitializedCallFunctionNode.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2013, Regents of the University of California
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.uci.python.nodes.calls;
+
+import com.oracle.truffle.api.*;
+import com.oracle.truffle.api.frame.*;
+
+import edu.uci.python.nodes.*;
+import edu.uci.python.runtime.datatypes.*;
+import edu.uci.python.runtime.standardtypes.*;
+
+public class UninitializedCallFunctionNode extends CallFunctionNode {
+
+    @Child protected PNode callee;
+
+    public UninitializedCallFunctionNode(PNode callee, PNode[] arguments, PNode[] keywords) {
+        super(arguments, keywords);
+        this.callee = adoptChild(callee);
+    }
+
+    @Override
+    public PNode getCallee() {
+        return callee;
+    }
+
+    @Override
+    public Object execute(VirtualFrame frame) {
+        CompilerAsserts.neverPartOfCompilation();
+        Object calleeObj = callee.execute(frame);
+
+        if (calleeObj instanceof PythonClass) {
+            CallConstructorNode specialized = new CallConstructorNode(getCallee(), arguments);
+            replace(specialized);
+            Object[] args = CallFunctionNode.executeArguments(frame, arguments);
+            return specialized.callConstructor(frame, (PythonClass) calleeObj, args);
+        } else if (calleeObj instanceof PCallable) {
+            PCallable callable = (PCallable) calleeObj;
+
+            if (callable.isBuiltin()) {
+                CallBuiltInFunctionNode callBuiltIn = CallBuiltInFunctionNodeFactory.create(callable, callable.getName(), arguments, keywords);
+                replace(callBuiltIn);
+                return callBuiltIn.doGeneric(frame);
+            } else if (keywords.length == 0) {
+                CallFunctionNoKeywordNode callFunction = new CallFunctionNoKeywordNode(callee, arguments);
+                replace(callFunction);
+                return callFunction.executeCall(frame, callable);
+            } else {
+                CallFunctionNode callFunction = CallFunctionNodeFactory.create(arguments, keywords, callee);
+                replace(callFunction);
+                return callFunction.doPCallable(frame, callable);
+            }
+        } else {
+            CallFunctionNode callFunction = CallFunctionNodeFactory.create(arguments, keywords, callee);
+            replace(callFunction);
+            callFunction.execute(frame);
+            return callFunction.doGeneric(frame, calleeObj);
+        }
+    }
+}
---
+++ graal/edu.uci.python.shell/src/edu/uci/python/shell/CustomConsole.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2013, Regents of the University of California
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.uci.python.shell;
+
+import java.io.*;
+
+import org.python.antlr.base.*;
+import org.python.core.*;
+import org.python.util.*;
+
+import edu.uci.python.nodes.translation.*;
+import edu.uci.python.nodes.truffle.*;
+import edu.uci.python.parser.*;
+import edu.uci.python.runtime.*;
+import edu.uci.python.runtime.Options;
+
+public class CustomConsole extends JLineConsole {
+
+    @Override
+    public void execfile(java.io.InputStream s, String name) {
+        PythonContext context = new PythonContext(new Options());
+        execfile(s, name, context);
+    }
+
+    public void execfile(java.io.InputStream s, String name, PythonContext context) {
+        setSystemState();
+
+        ASTInterpreter.init(false);
+        PythonParseResult result = parseToAST(s, name, CompileMode.exec, cflags, context);
+
+        if (Options.PrintAST) {
+            printBanner("Before Specialization");
+            result.printAST();
+        }
+
+        if (Options.VisualizedAST) {
+            result.visualizeToNetwork();
+        }
+
+        ASTInterpreter.interpret(result, false);
+
+        if (Options.PrintAST) {
+            printBanner("After Specialization");
+            result.printAST();
+        }
+
+        if (Options.VisualizedAST) {
+            result.visualizeToNetwork();
+        }
+
+        Py.flushLine();
+    }
+
+    /**
+     * Truffle: Parse input program to AST that is ready to interpret itself.
+     */
+    public static PythonParseResult parseToAST(InputStream istream, String filename, CompileMode kind, CompilerFlags cflags, PythonContext context) {
+        mod node = ParserFacade.parse(istream, kind, filename, cflags);
+        TranslationEnvironment environment = new TranslationEnvironment(node);
+        PythonTreeProcessor ptp = new PythonTreeProcessor(environment);
+        node = ptp.process(node);
+
+        PythonTreeTranslator ptt = new PythonTreeTranslator(environment, context);
+        PythonParseResult result = ptt.translate(node);
+        return result;
+    }
+
+    public static void printBanner(String phase) {
+        // CheckStyle: stop system..print check
+        System.out.println("============= " + phase + " ============= ");
+        // CheckStyle: resume system..print check
+    }
+}
