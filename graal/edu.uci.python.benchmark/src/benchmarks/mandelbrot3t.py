c019dabaa8080b587c0e3b4c236826a98e089d8f
Wei Zhang <ndrzmansn@gmail.com>
2013-10-11 15:30:42 -0700

    Clean up

---
+++ graal/edu.uci.python.benchmark/src/benchmarks/mandelbrot3t.py
@@ -0,0 +1,62 @@
+# The Computer Language Benchmarks Game
+# http://shootout.alioth.debian.org/
+#
+# contributed by Tupteq
+# 2to3 - fixed by Daniele Varrazzo
+
+#from __future__ import print_function
+import sys
+import time
+
+def main(num):
+    cout = sys.stdout.write
+    #size = float(sys.argv[1])
+    size_int = int(num)
+    xr_size = range(size_int)
+    xr_iter = range(50)
+    bit = 128
+    byte_acc = 0
+
+    cout("P4\n%d %d\n" % (size_int, size_int))
+
+    loop(size_int, xr_size, xr_iter, bit, byte_acc, cout)
+    size = float(size_int)
+    for y in xr_size:
+        fy = 2j * y / size - 1j
+        for x in xr_size:
+            z = 0j
+            c = 2. * x / size - 1.5 + fy
+
+            for i in xr_iter:
+                z = z * z + c
+                if abs(z) >= 2.0:
+                    break
+            else:
+                byte_acc += bit
+
+            if bit > 1:
+                bit >>= 1
+            else:
+                cout(chr(byte_acc))
+                bit = 128
+                byte_acc = 0
+
+
+        # zwei: disabled this block. Because it was never executed within the parameter value range that we use.
+        # All the nodes in this block is usually never specialized, which is a problem for partial evaluation.
+        # if bit != 128:
+        #     cout(chr(byte_acc))
+        #     bit = 128
+        #     byte_acc = 0
+
+# warm up
+for run in range(6):
+    main(300)
+    print()
+
+print("Start timing...")
+start = time.time()
+main(sys.argv[1])
+duration = "%.3f\n" % (time.time() - start)
+print()
+print("mandelbrot: " + duration)
