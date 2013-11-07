9aeca0510ba9ae68855b699de78e534cb57b6208
Gulfem Savrun <gulfemsavrun@gmail.com>
2013-11-06 18:12:52 -0800

    Created a junit test for builtins

---
+++ graal/edu.uci.python.runtime/src/edu/uci/python/runtime/datatypes/PGenerator.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2013, Regents of the University of California
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.uci.python.runtime.datatypes;
+
+import java.util.*;
+
+import com.oracle.truffle.api.*;
+import com.oracle.truffle.api.frame.*;
+
+public class PGenerator extends PCallable implements Iterator<Object>, Iterable<Object> {
+
+    private final CallTarget callTarget;
+    private final FrameDescriptor frameDescriptor;
+
+    private boolean hasNext = true;
+
+    public PGenerator(String name, CallTarget callTarget, FrameDescriptor frameDescriptor) {
+        super(name);
+        this.callTarget = callTarget;
+        this.frameDescriptor = frameDescriptor;
+    }
+
+    public FrameDescriptor getFrameDescriptor() {
+        return frameDescriptor;
+    }
+
+    @Override
+    public Object call(PackedFrame caller, Object[] args) {
+        return callTarget.call(caller, new PArguments(PNone.NONE, args));
+    }
+
+    @Override
+    public Object call(PackedFrame caller, Object[] args, Object[] keywords) {
+        return callTarget.call(caller, new PArguments(PNone.NONE, args));
+    }
+
+    /**
+     * FIXME: this class is being rewritten (very rough).
+     */
+    @Override
+    public Object next() {
+        return call(null, null);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return hasNext;
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<Object> iterator() {
+        return this;
+    }
+}
