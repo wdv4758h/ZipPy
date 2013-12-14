ecd7c8aabbfeed2f9e01fba88432d8bcce2aa5c4
Wei Zhang <ndrzmansn@gmail.com>
2013-12-13 17:33:23 -0800

    GeneratorExpressionDefinition does not have to iterate over its produced PGenerator and return a PSequence in some cases. Make specialization cases for PIterator in builtin functions like frozenset and dict

---
+++ graal/edu.uci.python.runtime/src/edu/uci/python/runtime/function/PArguments.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2013, Regents of the University of California
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.uci.python.runtime.function;
+
+import com.oracle.truffle.api.*;
+import com.oracle.truffle.api.frame.*;
+
+import edu.uci.python.runtime.datatypes.*;
+
+public class PArguments extends Arguments {
+
+    public static final Object[] EMPTY_ARGUMENTS_ARRAY = new Object[0];
+    public static final PArguments EMPTY_ARGUMENT = new PArguments(null);
+
+    private final MaterializedFrame declarationFrame;
+    private final MaterializedFrame generatorFrame;
+
+    private final Object self;
+    private final Object[] arguments;
+    private final PKeyword[] keywords;
+
+    public PArguments(Object self, MaterializedFrame declarationFrame, MaterializedFrame generatorFrame, Object[] arguments, PKeyword[] keywords) {
+        this.self = self;
+        this.declarationFrame = declarationFrame;
+        this.generatorFrame = generatorFrame;
+        this.arguments = arguments;
+        this.keywords = keywords;
+    }
+
+    public PArguments(MaterializedFrame declarationFrame) {
+        this(null, declarationFrame, null, EMPTY_ARGUMENTS_ARRAY, PKeyword.EMPTY_KEYWORDS);
+    }
+
+    public PArguments(MaterializedFrame declarationFrame, MaterializedFrame generatorFrame, Object[] arguments) {
+        this(null, declarationFrame, generatorFrame, arguments, PKeyword.EMPTY_KEYWORDS);
+    }
+
+    public PArguments(Object self, MaterializedFrame declarationFrame, Object[] arguments) {
+        this(self, declarationFrame, null, arguments, PKeyword.EMPTY_KEYWORDS);
+    }
+
+    public static PArguments get(Frame frame) {
+        return frame.getArguments(PArguments.class);
+    }
+
+    public MaterializedFrame getGeneratorFrame() {
+        assert generatorFrame != null;
+        return generatorFrame;
+    }
+
+    public Object getSelf() {
+        return self;
+    }
+
+    public final Object[] getArgumentsArray() {
+        return CompilerDirectives.unsafeCast(arguments, Object[].class, true);
+    }
+
+    public MaterializedFrame getDeclarationFrame() {
+        return CompilerDirectives.unsafeFrameCast(declarationFrame);
+    }
+
+    public final Object getArgument(int index) {
+        if (index >= arguments.length) {
+            return PNone.NONE;
+        }
+
+        return arguments[index];
+    }
+
+    public PKeyword getKeyword(String name) {
+        for (int i = 0; i < keywords.length; i++) {
+            PKeyword keyword = keywords[i];
+            if (keyword.getName().equals(name)) {
+                return keyword;
+            }
+        }
+
+        return null;
+    }
+
+    public PKeyword[] getKeywords() {
+        return keywords;
+    }
+
+    public int getLength() {
+        return arguments.length;
+    }
+
+}
