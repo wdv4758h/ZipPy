7e135f6ff8a3471573b264084079b1e6eecce873
Gulfem Savrun <gulfemsavrun@gmail.com>
2014-06-06 17:38:36 -0700

    Profiled WriteLocal, ReadLocal, GetAttribute, and SetAttribute nodes, added an option for finding nodes with empty source sections, and added PythonWriteNodeWrapperNode for dealing ForNode while profiling

---
+++ graal/edu.uci.python.nodes/src/edu/uci/python/nodes/expression/BinaryComparisonNode.java
@@ -0,0 +1,401 @@
+/*
+ * Copyright (c) 2013, Regents of the University of California
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.uci.python.nodes.expression;
+
+import java.math.*;
+
+import com.oracle.truffle.api.dsl.Generic;
+import com.oracle.truffle.api.dsl.Specialization;
+
+import edu.uci.python.runtime.datatype.*;
+import edu.uci.python.runtime.sequence.*;
+import edu.uci.python.runtime.sequence.storage.*;
+import edu.uci.python.runtime.standardtype.*;
+
+public abstract class BinaryComparisonNode extends BinaryOpNode {
+
+    public abstract static class EqualNode extends BinaryComparisonNode {
+
+        @Specialization
+        boolean doInteger(int left, int right) {
+            return left == right;
+        }
+
+        @Specialization
+        boolean doBigInteger(BigInteger left, BigInteger right) {
+            return left.equals(right);
+        }
+
+        @Specialization
+        boolean doDouble(double left, double right) {
+            return left == right;
+        }
+
+        @Specialization
+        boolean doComplex(PComplex left, PComplex right) {
+            return left.equals(right);
+        }
+
+        @Specialization
+        boolean doString(String left, String right) {
+            return left.equals(right);
+        }
+
+        @Specialization
+        boolean doPList(PList left, PList right) {
+            return left.equals(right);
+        }
+
+        @Specialization
+        boolean doPDict(PDict left, PDict right) {
+            return left.equals(right);
+        }
+
+        @Specialization
+        public boolean doPythonClass(PythonClass left, PythonClass right) {
+            return left.equals(right);
+        }
+
+        @SuppressWarnings("unused")
+        @Specialization
+        public boolean doNone(Object left, PNone none) {
+            return left == PNone.NONE;
+        }
+
+        /**
+         * This is a fix for comparisons involving a PyInteger.
+         *
+         * @param left
+         * @param right
+         * @return comparison result
+         */
+        @Generic
+        public boolean doGeneric(Object left, Object right) {
+            return left.equals(right);
+        }
+    }
+
+    public abstract static class NotEqualNode extends BinaryComparisonNode {
+
+        @Specialization
+        boolean doInteger(int left, int right) {
+            return left != right;
+        }
+
+        @Specialization
+        boolean doBigInteger(BigInteger left, BigInteger right) {
+            return !left.equals(right);
+        }
+
+        @Specialization
+        boolean doDouble(double left, double right) {
+            return left != right;
+        }
+
+        @Specialization
+        boolean doComplex(PComplex left, PComplex right) {
+            return left.notEqual(right);
+        }
+
+        @Specialization
+        boolean doString(String left, String right) {
+            return !left.equals(right);
+        }
+
+        @Specialization
+        boolean doPList(PList left, PList right) {
+            return !left.equals(right);
+        }
+
+        @Specialization
+        boolean doPDict(PDict left, PDict right) {
+            return !left.equals(right);
+        }
+
+        @Specialization
+        public boolean doPythonClass(PythonClass left, PythonClass right) {
+            return !left.equals(right);
+        }
+    }
+
+    public abstract static class LessThanNode extends BinaryComparisonNode {
+
+        @Specialization
+        boolean doInteger(int left, int right) {
+            return left < right;
+        }
+
+        @Specialization
+        boolean doBigInteger(BigInteger left, BigInteger right) {
+            return left.compareTo(right) < 0;
+        }
+
+        @Specialization
+        boolean doDouble(double left, double right) {
+            return left < right;
+        }
+
+        @Specialization
+        boolean doComplex(PComplex left, PComplex right) {
+            return left.lessThan(right);
+        }
+
+        @Specialization
+        boolean doString(String left, String right) {
+            return left.compareTo(right) < 0;
+        }
+
+        @Specialization
+        boolean doTruffleSequence(PSequence left, PSequence right) {
+            return left.lessThan(right);
+        }
+    }
+
+    public abstract static class LessThanEqualNode extends BinaryComparisonNode {
+
+        @Specialization
+        boolean doInteger(int left, int right) {
+            return left <= right;
+        }
+
+        @Specialization
+        boolean doBigInteger(BigInteger left, BigInteger right) {
+            return left.compareTo(right) <= 0;
+        }
+
+        @Specialization
+        boolean doDouble(double left, double right) {
+            return left <= right;
+        }
+
+        @Specialization
+        boolean doComplex(PComplex left, PComplex right) {
+            return left.lessEqual(right);
+        }
+
+        @Specialization
+        boolean doString(String left, String right) {
+            return left.compareTo(right) <= 0;
+        }
+
+        @Specialization
+        boolean doPBaseSet(PBaseSet left, PBaseSet right) {
+            return left.isSubset(right);
+        }
+
+    }
+
+    public abstract static class GreaterThanNode extends BinaryComparisonNode {
+
+        @Specialization
+        boolean doInteger(int left, int right) {
+            return left > right;
+        }
+
+        @Specialization
+        boolean doBigInteger(BigInteger left, BigInteger right) {
+            return left.compareTo(right) > 0;
+        }
+
+        @Specialization
+        boolean doDouble(double left, double right) {
+            return left > right;
+        }
+
+        @Specialization
+        boolean doComplex(PComplex left, PComplex right) {
+            return left.greaterThan(right);
+        }
+
+        @Specialization
+        boolean doString(String left, String right) {
+            return left.compareTo(right) > 0;
+        }
+    }
+
+    public abstract static class GreaterThanEqualNode extends BinaryComparisonNode {
+
+        @Specialization
+        boolean doInteger(int left, int right) {
+            return left >= right;
+        }
+
+        @Specialization
+        boolean doBigInteger(BigInteger left, BigInteger right) {
+            return left.compareTo(right) >= 0;
+        }
+
+        @Specialization
+        boolean doDouble(double left, double right) {
+            return left >= right;
+        }
+
+        @Specialization
+        boolean doComplex(PComplex left, PComplex right) {
+            return left.greaterEqual(right);
+        }
+
+        @Specialization
+        boolean doString(String left, String right) {
+            return left.compareTo(right) >= 0;
+        }
+    }
+
+    public abstract static class IsNode extends BinaryComparisonNode {
+
+        @Specialization(order = 3)
+        boolean doInteger(int left, int right) {
+            return left == right;
+        }
+
+        @Specialization(order = 4)
+        boolean doBigInteger(BigInteger left, BigInteger right) {
+            return left.compareTo(right) == 0;
+        }
+
+        @Specialization(order = 5)
+        boolean doDouble(double left, double right) {
+            return left == right;
+        }
+
+        @SuppressWarnings("unused")
+        @Specialization(order = 10)
+        boolean doLeftPNone(PNone left, Object right) {
+            return PNone.NONE == right;
+        }
+
+        @SuppressWarnings("unused")
+        @Specialization(order = 11)
+        boolean doRightPNone(Object left, PNone right) {
+            return left == PNone.NONE;
+        }
+
+        @Generic
+        public boolean doGeneric(Object left, Object right) {
+            return left.equals(right);
+        }
+    }
+
+    public abstract static class IsNotNode extends BinaryComparisonNode {
+
+        @Specialization(order = 1)
+        boolean doInteger(int left, int right) {
+            return left != right;
+        }
+
+        @Specialization(order = 2)
+        boolean doBigInteger(BigInteger left, BigInteger right) {
+            return left.compareTo(right) != 0;
+        }
+
+        @Specialization(order = 3)
+        boolean doDouble(double left, double right) {
+            return left != right;
+        }
+
+        @SuppressWarnings("unused")
+        @Specialization(order = 10)
+        public boolean doLeftPNone(PNone left, Object right) {
+            return PNone.NONE != right;
+        }
+
+        @SuppressWarnings("unused")
+        @Specialization(order = 11)
+        public boolean doRightPNone(Object left, PNone right) {
+            return left != PNone.NONE;
+        }
+
+        @Generic
+        public boolean doGeneric(Object left, Object right) {
+            return !left.equals(right);
+        }
+    }
+
+    public abstract static class InNode extends BinaryComparisonNode {
+
+        @Specialization
+        public boolean doBaseSet(Object left, PBaseSet right) {
+            return right.contains(left);
+        }
+
+        @Specialization
+        public boolean doPSequence(Object left, PSequence right) {
+            return right.index(left) != -1;
+        }
+
+        @Specialization
+        public boolean doPDictionary(Object left, PDict right) {
+            return right.hasKey(left);
+        }
+    }
+
+    public abstract static class NotInNode extends BinaryComparisonNode {
+
+        @Specialization(order = 5)
+        public boolean doBaseSet(Object left, PBaseSet right) {
+            return !right.contains(left);
+        }
+
+        @SuppressWarnings("unused")
+        @Specialization(order = 10, guards = "is2ndEmptyStorage")
+        public boolean doPListEmpty(Object left, PList right) {
+            return true;
+        }
+
+        @Specialization(order = 11, guards = "is2ndIntStorage")
+        public boolean doPListInt(int left, PList right) {
+            IntSequenceStorage store = (IntSequenceStorage) right.getStorage();
+            return store.indexOfInt(left) == -1;
+        }
+
+        @Specialization(order = 12, guards = "is2ndDoubleStorage")
+        public boolean doPListDouble(double left, PList right) {
+            DoubleSequenceStorage store = (DoubleSequenceStorage) right.getStorage();
+            return store.indexOfDouble(left) == -1;
+        }
+
+        @Specialization(order = 13, guards = "is2ndObjectStorage")
+        public boolean doPListObject(Object left, PList right) {
+            ObjectSequenceStorage store = (ObjectSequenceStorage) right.getStorage();
+            return store.index(left) == -1;
+        }
+
+        @Specialization(order = 15)
+        public boolean doPList(Object left, PList right) {
+            return right.index(left) == -1;
+        }
+
+        @Specialization(order = 19)
+        public boolean doPSequence(Object left, PSequence right) {
+            return right.index(left) == -1;
+        }
+
+        @Specialization(order = 20)
+        public boolean doPDictionary(Object left, PDict right) {
+            return !right.hasKey(left);
+        }
+    }
+
+}
---
+++ graal/edu.uci.python.parser/src/edu/uci/python/parser/ProfilerTranslator.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2014, Regents of the University of California
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.uci.python.parser;
+
+import com.oracle.truffle.api.nodes.*;
+
+import edu.uci.python.nodes.*;
+import edu.uci.python.nodes.expression.*;
+import edu.uci.python.nodes.frame.*;
+import edu.uci.python.nodes.object.*;
+import edu.uci.python.nodes.profiler.*;
+import edu.uci.python.nodes.subscript.*;
+import edu.uci.python.runtime.*;
+
+public class ProfilerTranslator implements NodeVisitor {
+
+    private final PythonNodeProber astProber;
+
+    public ProfilerTranslator(PythonContext context) {
+        this.astProber = new PythonNodeProber(context);
+    }
+
+    public void translate(PythonParseResult parseResult, RootNode root) {
+        root.accept(this);
+
+        for (RootNode functionRoot : parseResult.getFunctionRoots()) {
+            functionRoot.accept(this);
+        }
+    }
+
+    @Override
+    public boolean visit(Node node) {
+        /**
+         * Profile binary operations:BinaryArithmeticNode, BinaryBitwiseNode, BinaryBooleanNode,
+         * BinaryComparisonNode, SubscriptLoadIndexNode, SubscriptLoadSliceNode, SubscriptDeleteNode
+         */
+        if (node instanceof BinaryArithmeticNode) {
+            createWrapperNode((PNode) node);
+        } else if (node instanceof BinaryBitwiseNode) {
+            createWrapperNode((PNode) node);
+        } else if (node instanceof BinaryBooleanNode) {
+            createWrapperNode((PNode) node);
+        } else if (node instanceof BinaryComparisonNode) {
+            createWrapperNode((PNode) node);
+        } else if (node instanceof SubscriptLoadIndexNode) {
+            createWrapperNode((PNode) node);
+        } else if (node instanceof SubscriptLoadSliceNode) {
+            createWrapperNode((PNode) node);
+        } else if (node instanceof SubscriptDeleteNode) {
+            createWrapperNode((PNode) node);
+        } else if (node instanceof SubscriptStoreIndexNode) {
+            createWrapperNode((PNode) node);
+        } else if (node instanceof SubscriptStoreSliceNode) {
+            createWrapperNode((PNode) node);
+        } else if (node instanceof WriteLocalVariableNode) {
+            createWriteNodeWrapperNode((PNode) node);
+        } else if (node instanceof ReadLocalVariableNode) {
+            createWrapperNode((PNode) node);
+        } else if (node instanceof SetAttributeNode) {
+            createWriteNodeWrapperNode((PNode) node);
+        } else if (node instanceof GetAttributeNode) {
+            createWrapperNode((PNode) node);
+        }
+
+// } else if (node instanceof ListComprehensionNode) {
+// createWrapperNode((PNode) node);
+// } else if (node instanceof IfNode) {
+// createWrapperNode((PNode) node);
+// }
+        return true;
+    }
+
+    private PythonWrapperNode createWrapperNode(PNode node) {
+        if (node.getSourceSection() == null) {
+            if (PythonOptions.TraceNodesWithoutSourceSection) {
+                ProfilerResultPrinter.addNodeEmptySourceSection(node);
+                return null;
+            } else {
+                throw new RuntimeException("Source is not assigned for " + node);
+            }
+        }
+
+        PythonWrapperNode wrapperNode = astProber.probeAsStatement(node);
+        node.replace(wrapperNode);
+        wrapperNode.adoptChildren();
+        return wrapperNode;
+    }
+
+    private PythonWriteNodeWrapperNode createWriteNodeWrapperNode(PNode node) {
+        if (node.getSourceSection() == null) {
+            if (PythonOptions.TraceNodesWithoutSourceSection) {
+                ProfilerResultPrinter.addNodeEmptySourceSection(node);
+                return null;
+            } else {
+                throw new RuntimeException("Source is not assigned for " + node);
+            }
+        }
+
+        PythonWriteNodeWrapperNode wrapperNode = astProber.probeAsWriteNode(node);
+        node.replace(wrapperNode);
+        wrapperNode.adoptChildren();
+        return wrapperNode;
+    }
+
+}
