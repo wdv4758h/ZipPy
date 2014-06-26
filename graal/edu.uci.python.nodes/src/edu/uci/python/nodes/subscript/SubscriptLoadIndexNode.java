9cd64d6876af0b1b110c3b1eed2276cc04762d0f
Wei Zhang <ndrzmansn@gmail.com>
2014-06-25 17:42:55 -0700

    Bounce check for ObjectSequenceStorage

---
+++ graal/edu.uci.python.nodes/src/edu/uci/python/nodes/subscript/SubscriptLoadIndexNode.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) 2013, Regents of the University of California
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.uci.python.nodes.subscript;
+
+import org.python.core.*;
+
+import com.oracle.truffle.api.*;
+import com.oracle.truffle.api.dsl.*;
+import com.oracle.truffle.api.frame.*;
+
+import edu.uci.python.nodes.*;
+import edu.uci.python.nodes.truffle.*;
+import edu.uci.python.runtime.array.*;
+import edu.uci.python.runtime.datatype.*;
+import edu.uci.python.runtime.object.*;
+import edu.uci.python.runtime.sequence.*;
+import edu.uci.python.runtime.sequence.storage.*;
+
+public abstract class SubscriptLoadIndexNode extends SubscriptLoadNode {
+
+    public PNode makeWriteNode(PNode rhs) {
+        return SubscriptStoreIndexNodeFactory.create(getPrimary(), getSlice(), rhs);
+    }
+
+    @Specialization(order = 0)
+    public String doString(String primary, int idx) {
+        int index = idx;
+
+        if (idx < 0) {
+            index += primary.length();
+        }
+
+        return charAtToString(primary, index);
+    }
+
+    private static String charAtToString(String primary, int index) {
+        char charactor = primary.charAt(index);
+        return new String(new char[]{charactor});
+    }
+
+    @Specialization(order = 1, guards = "isIntStorage")
+    public int doPListInt(PList primary, int idx) {
+        final IntSequenceStorage store = (IntSequenceStorage) primary.getStorage();
+        int index = SequenceUtil.normalizeIndex(idx, store.length());
+        return store.getIntItemInBound(index);
+    }
+
+    @Specialization(order = 2, guards = "isDoubleStorage")
+    public double doPListDouble(PList primary, int idx) {
+        final DoubleSequenceStorage store = (DoubleSequenceStorage) primary.getStorage();
+        int index = SequenceUtil.normalizeIndex(idx, store.length());
+        return store.getDoubleItemInBound(index);
+    }
+
+    @Specialization(order = 3, guards = "isObjectStorage")
+    public Object doPListObject(PList primary, int idx) {
+        final ObjectSequenceStorage store = (ObjectSequenceStorage) primary.getStorage();
+        int index = SequenceUtil.normalizeIndex(idx, store.length());
+        return store.getItemInBound(index);
+    }
+
+    @Specialization(order = 5)
+    public Object doPList(PList list, int idx) {
+        return list.getItem(idx);
+    }
+
+    @Specialization(order = 7)
+    public Object doPTuple(PTuple tuple, int idx) {
+        return tuple.getItem(idx);
+    }
+
+    @Specialization(order = 10)
+    public Object doPRange(PRange primary, int idx) {
+        return primary.getItem(idx);
+    }
+
+    /**
+     * PDict lookup using key.
+     */
+    @Specialization(order = 11)
+    public Object doPDict(PDict primary, Object key) {
+        final Object result = primary.getItem(key);
+        assert result != null;
+        return result;
+    }
+
+    /**
+     * Unboxed array reads.
+     */
+    @Specialization(order = 12)
+    public int doPIntArray(PIntArray primary, int index) {
+        return primary.getIntItemInBound(index);
+    }
+
+    @Specialization(order = 13)
+    public double doPDoubleArray(PDoubleArray primary, int index) {
+        return primary.getDoubleItemInBound(index);
+    }
+
+    @Specialization(order = 14)
+    public char doPCharArray(PCharArray primary, int index) {
+        return primary.getCharItemInBound(index);
+    }
+
+    @Specialization(order = 15)
+    public Object doPArray(PArray primary, int index) {
+        return primary.getItem(index);
+    }
+
+    /**
+     * zwei: PythonTypesUtil does not unbox PyList. Instead we perform inplace update on PyList.
+     * This avoid unwated data strcture duplication and actually updates a PyList imported from
+     * Jython. As soon as we never have to actually read an imported PyList, this should be gone.
+     */
+    @Specialization(order = 19)
+    public Object doPyList(PyObject primary, int index) {
+        CompilerAsserts.neverPartOfCompilation();
+
+        PyList list = (PyList) primary;
+        Object value = list.get(index);
+
+        if (value instanceof PyObject) {
+            return PythonTypesUtil.unboxPyObject((PyObject) value);
+        }
+
+        return value;
+    }
+
+    @Specialization(order = 20)
+    public Object doSpecialInt(VirtualFrame frame, PythonObject primary, int index) {
+        return doSpecialMethodCall(frame, "__getitem__", primary, index);
+    }
+
+    @Specialization(order = 21)
+    public Object doSpecialObject(VirtualFrame frame, PythonObject primary, Object index) {
+        return doSpecialMethodCall(frame, "__getitem__", primary, index);
+    }
+
+}
---
+++ graal/edu.uci.python.parser/src/edu/uci/python/parser/ProfilerTranslator.java
@@ -0,0 +1,205 @@
+/*
+ * Copyright (c) 2014, Regents of the University of California
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.uci.python.parser;
+
+import com.oracle.truffle.api.nodes.*;
+
+import edu.uci.python.nodes.*;
+import edu.uci.python.nodes.control.*;
+import edu.uci.python.nodes.expression.*;
+import edu.uci.python.nodes.frame.*;
+import edu.uci.python.nodes.function.*;
+import edu.uci.python.nodes.object.*;
+import edu.uci.python.nodes.profiler.*;
+import edu.uci.python.nodes.subscript.*;
+import edu.uci.python.runtime.*;
+
+public class ProfilerTranslator implements NodeVisitor {
+
+    private final PythonNodeProber astProber;
+
+    public ProfilerTranslator(PythonContext context) {
+        this.astProber = new PythonNodeProber(context);
+    }
+
+    public void translate(PythonParseResult parseResult, RootNode root) {
+        root.accept(this);
+
+        for (RootNode functionRoot : parseResult.getFunctionRoots()) {
+            functionRoot.accept(this);
+        }
+    }
+
+    @Override
+    public boolean visit(Node node) {
+        if (PythonOptions.ProfileCalls) {
+            if (node instanceof FunctionRootNode) {
+                FunctionRootNode rootNode = (FunctionRootNode) node;
+                PNode body = rootNode.getBody();
+                createCallNodeWrapper(body);
+            }
+        } else if (PythonOptions.ProfileIfNodes) {
+            if (node instanceof IfNode) {
+                IfNode ifNode = (IfNode) node;
+                /**
+                 * 1) If node has a condition node which is a castToBooleanNode. CastToBooleanNode
+                 * has a child which is the actual condition. So be careful while profiling if
+                 * nodes. Do not profile if nodes and condition nodes together, because prober
+                 * increments counter twice for the same node. <br>
+                 * 2) If nodes in a comprehension does not yet have a source section, so such if
+                 * nodes are not profiled.
+                 */
+                if (hasSourceSection(ifNode)) {
+                    CastToBooleanNode castToBooleanNode = ifNode.getCondition();
+                    PNode conditionNode = castToBooleanNode.getOperand();
+                    createConditionNodeWrapper(conditionNode);
+                    PNode thenNode = ifNode.getThen();
+                    PNode elseNode = ifNode.getElse();
+                    createThenNodeWrapper(thenNode);
+                    /**
+                     * Only create a wrapper node if an else exists.
+                     */
+                    if (!(elseNode instanceof EmptyNode)) {
+                        createElseNodeWrapper(elseNode);
+                    }
+                }
+            }
+        } else if (PythonOptions.ProfileNodes) {
+            /**
+             * Profile binary operations:BinaryArithmeticNode, BinaryBitwiseNode, BinaryBooleanNode,
+             * BinaryComparisonNode, SubscriptLoadIndexNode, SubscriptLoadSliceNode,
+             * SubscriptDeleteNode
+             */
+            if (node instanceof BinaryArithmeticNode) {
+                createWrapper((PNode) node);
+            } else if (node instanceof BinaryBitwiseNode) {
+                createWrapper((PNode) node);
+            } else if (node instanceof BinaryBooleanNode) {
+                createWrapper((PNode) node);
+            } else if (node instanceof BinaryComparisonNode) {
+                createWrapper((PNode) node);
+            } else if (node instanceof SubscriptLoadIndexNode) {
+                createWrapper((PNode) node);
+            } else if (node instanceof SubscriptLoadSliceNode) {
+                createWrapper((PNode) node);
+            } else if (node instanceof SubscriptDeleteNode) {
+                createWrapper((PNode) node);
+            } else if (node instanceof SubscriptStoreIndexNode) {
+                createWrapper((PNode) node);
+            } else if (node instanceof SubscriptStoreSliceNode) {
+                createWrapper((PNode) node);
+            } else if (node instanceof WriteLocalVariableNode) {
+                createWriteNodeWrapper((PNode) node);
+            } else if (node instanceof ReadLocalVariableNode) {
+                createWrapper((PNode) node);
+            } else if (node instanceof SetAttributeNode) {
+                createWriteNodeWrapper((PNode) node);
+            } else if (node instanceof GetAttributeNode) {
+                createWrapper((PNode) node);
+            } else if (node instanceof BreakNode) {
+                createWrapper((PNode) node);
+            } else if (node instanceof ContinueNode) {
+                createWrapper((PNode) node);
+            }
+        }
+
+        return true;
+    }
+
+    private PythonWrapperNode createWrapper(PNode node) {
+        if (hasSourceSection(node)) {
+            PythonWrapperNode wrapperNode = astProber.probeAsStatement(node);
+            node.replace(wrapperNode);
+            wrapperNode.adoptChildren();
+            return wrapperNode;
+        }
+
+        return null;
+    }
+
+    private PythonWriteNodeWrapperNode createWriteNodeWrapper(PNode node) {
+        if (hasSourceSection(node)) {
+            PythonWriteNodeWrapperNode wrapperNode = astProber.probeAsWriteNode(node);
+            node.replace(wrapperNode);
+            wrapperNode.adoptChildren();
+            return wrapperNode;
+        }
+
+        return null;
+    }
+
+    private PythonWrapperNode createCallNodeWrapper(PNode node) {
+        if (hasSourceSection(node)) {
+            PythonWrapperNode wrapperNode = astProber.probeAsCall(node);
+            node.replace(wrapperNode);
+            wrapperNode.adoptChildren();
+            return wrapperNode;
+        }
+
+        return null;
+    }
+
+    private PythonWrapperNode createConditionNodeWrapper(PNode node) {
+        if (hasSourceSection(node)) {
+            PythonWrapperNode wrapperNode = astProber.probeAsCondition(node);
+            node.replace(wrapperNode);
+            wrapperNode.adoptChildren();
+            return wrapperNode;
+        }
+
+        return null;
+    }
+
+    private PythonWrapperNode createThenNodeWrapper(PNode node) {
+        if (hasSourceSection(node)) {
+            PythonWrapperNode wrapperNode = astProber.probeAsThen(node);
+            node.replace(wrapperNode);
+            wrapperNode.adoptChildren();
+            return wrapperNode;
+        }
+
+        return null;
+    }
+
+    private PythonWrapperNode createElseNodeWrapper(PNode node) {
+        if (hasSourceSection(node)) {
+            PythonWrapperNode wrapperNode = astProber.probeAsElse(node);
+            node.replace(wrapperNode);
+            wrapperNode.adoptChildren();
+            return wrapperNode;
+        }
+
+        return null;
+    }
+
+    private static boolean hasSourceSection(PNode node) {
+        if (node.getSourceSection() == null) {
+            ProfilerResultPrinter.addNodeEmptySourceSection(node);
+            return false;
+        }
+
+        return true;
+    }
+}
