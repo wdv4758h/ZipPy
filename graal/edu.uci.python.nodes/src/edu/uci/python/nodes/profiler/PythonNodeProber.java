b26863d9e2b4f4f9c1d679cce71196b322c242af
Wei Zhang <ndrzmansn@gmail.com>
2014-07-02 23:49:01 -0700

    Rename

---
+++ graal/edu.uci.python.nodes/src/edu/uci/python/nodes/profiler/PythonNodeProber.java
@@ -0,0 +1,172 @@
+/*
+ * Copyright (c) 2014, Regents of the University of California
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.uci.python.nodes.profiler;
+
+import java.util.*;
+
+import com.oracle.truffle.api.instrument.*;
+import com.oracle.truffle.api.nodes.*;
+
+import edu.uci.python.nodes.*;
+import edu.uci.python.runtime.*;
+
+/**
+ * @author Gulfem
+ */
+
+public class PythonNodeProber implements ASTNodeProber {
+
+    private final PythonContext context;
+
+    private static Map<PythonWrapperNode, ProfilerInstrument> wrapperToInstruments = new HashMap<>();
+    private static Map<PythonWrapperNode, ProfilerInstrument> callWrapperToInstruments = new HashMap<>();
+    private static Map<PythonWrapperNode, ProfilerInstrument> conditionWrapperToInstruments = new HashMap<>();
+    private static Map<PythonWrapperNode, ProfilerInstrument> thenWrapperToInstruments = new HashMap<>();
+    private static Map<PythonWrapperNode, ProfilerInstrument> elseWrapperToInstruments = new HashMap<>();
+
+    public PythonNodeProber(PythonContext context) {
+        this.context = context;
+    }
+
+    public Node probeAs(Node astNode, PhylumTag tag, Object... args) {
+        return astNode;
+    }
+
+    public PythonWrapperNode probeAsStatement(PNode node) {
+        PythonWrapperNode wrapper;
+        if (node instanceof PythonWrapperNode) {
+            wrapper = (PythonWrapperNode) node;
+        } else {
+            wrapper = new PythonWrapperNode(context, node);
+            wrapper.getProbe().tagAs(StandardTag.STATEMENT);
+            wrapper.assignSourceSection(node.getSourceSection());
+        }
+
+        ProfilerInstrument profilerInstrument = new ProfilerInstrument();
+        wrapper.getProbe().addInstrument(profilerInstrument);
+        wrapperToInstruments.put(wrapper, profilerInstrument);
+        return wrapper;
+    }
+
+    public PythonWriteNodeWrapperNode probeAsWriteNode(PNode node) {
+        PythonWriteNodeWrapperNode wrapper;
+        if (node instanceof PythonWriteNodeWrapperNode) {
+            wrapper = (PythonWriteNodeWrapperNode) node;
+        } else {
+            wrapper = new PythonWriteNodeWrapperNode(context, node);
+            wrapper.getProbe().tagAs(StandardTag.STATEMENT);
+            wrapper.assignSourceSection(node.getSourceSection());
+        }
+
+        ProfilerInstrument profilerInstrument = new ProfilerInstrument();
+        wrapper.getProbe().addInstrument(profilerInstrument);
+        wrapperToInstruments.put(wrapper, profilerInstrument);
+        return wrapper;
+    }
+
+    public PythonWrapperNode probeAsCall(PNode node) {
+        PythonWrapperNode wrapper;
+        if (node instanceof PythonWrapperNode) {
+            wrapper = (PythonWrapperNode) node;
+        } else {
+            wrapper = new PythonWrapperNode(context, node);
+            wrapper.getProbe().tagAs(StandardTag.CALL);
+            wrapper.assignSourceSection(node.getSourceSection());
+        }
+
+        ProfilerInstrument profilerInstrument = new ProfilerInstrument();
+        wrapper.getProbe().addInstrument(profilerInstrument);
+        callWrapperToInstruments.put(wrapper, profilerInstrument);
+        return wrapper;
+    }
+
+    public PythonWrapperNode probeAsCondition(PNode node) {
+        PythonWrapperNode wrapper;
+        if (node instanceof PythonWrapperNode) {
+            wrapper = (PythonWrapperNode) node;
+        } else {
+            wrapper = new PythonWrapperNode(context, node);
+            wrapper.getProbe().tagAs(StandardTag.STATEMENT);
+            wrapper.assignSourceSection(node.getSourceSection());
+        }
+
+        ProfilerInstrument profilerInstrument = new ProfilerInstrument();
+        wrapper.getProbe().addInstrument(profilerInstrument);
+        conditionWrapperToInstruments.put(wrapper, profilerInstrument);
+        return wrapper;
+    }
+
+    public PythonWrapperNode probeAsThen(PNode node) {
+        PythonWrapperNode wrapper;
+        if (node instanceof PythonWrapperNode) {
+            wrapper = (PythonWrapperNode) node;
+        } else {
+            wrapper = new PythonWrapperNode(context, node);
+            wrapper.getProbe().tagAs(StandardTag.STATEMENT);
+            wrapper.assignSourceSection(node.getSourceSection());
+        }
+
+        ProfilerInstrument profilerInstrument = new ProfilerInstrument();
+        wrapper.getProbe().addInstrument(profilerInstrument);
+        thenWrapperToInstruments.put(wrapper, profilerInstrument);
+        return wrapper;
+    }
+
+    public PythonWrapperNode probeAsElse(PNode node) {
+        PythonWrapperNode wrapper;
+        if (node instanceof PythonWrapperNode) {
+            wrapper = (PythonWrapperNode) node;
+        } else {
+            wrapper = new PythonWrapperNode(context, node);
+            wrapper.getProbe().tagAs(StandardTag.STATEMENT);
+            wrapper.assignSourceSection(node.getSourceSection());
+        }
+
+        ProfilerInstrument profilerInstrument = new ProfilerInstrument();
+        wrapper.getProbe().addInstrument(profilerInstrument);
+        elseWrapperToInstruments.put(wrapper, profilerInstrument);
+        return wrapper;
+    }
+
+    public static Map<PythonWrapperNode, ProfilerInstrument> getWrapperToInstruments() {
+        return wrapperToInstruments;
+    }
+
+    public static Map<PythonWrapperNode, ProfilerInstrument> getCallWrapperToInstruments() {
+        return callWrapperToInstruments;
+    }
+
+    public static Map<PythonWrapperNode, ProfilerInstrument> getConditionWrapperToInstruments() {
+        return conditionWrapperToInstruments;
+    }
+
+    public static Map<PythonWrapperNode, ProfilerInstrument> getThenWrapperToInstruments() {
+        return thenWrapperToInstruments;
+    }
+
+    public static Map<PythonWrapperNode, ProfilerInstrument> getElseWrapperToInstruments() {
+        return elseWrapperToInstruments;
+    }
+}
